059efcbd8016493c6296aea6a080265b0d29a5d3
Li <thuit@users.noreply.github.com>
2014-03-23 16:15:19 +0800

    Update Test1.java

--- Day2/src/Test1.java
+++ Day2/src/Test1.java
@@ -4,7 +4,7 @@
 //题目：判断[101, 200]有多少个素数，并输出所有素数。（素数：只能被1和它本身整除的正整数，1不是素数）
 public class Test1 {
     public static void main(String[] args) {
-//test
+
         int counter = 0;
 
         for (int i = 2; i < 10; i++) {
