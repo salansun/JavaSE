e590aae12c648446efa1056101d316862c117897
teacher <teacher@tsinghua.edu.cn>
2014-03-29 10:52:57 +0800

    3.29

---
+++ Day3/src/Human.java
@@ -0,0 +1,27 @@
+//类 = 域 + 方法
+//class = field(s) + method(s)
+//交通工具
+public class Human {
+
+    //    域
+    String name;
+    int age;
+    double weight;
+    double height;
+    char sex;//m, f
+
+    //    方法
+    void study(int i, double d, String s) {
+//        return;
+    }
+
+    void work() {
+
+    }
+
+    String exam(int i) {
+        i = 1;
+        return "passed";
+
+    }
+}
