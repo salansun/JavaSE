bfd314b8878580d8e9a3fbdc09ea752ef1f0db94
tester <test@tsinghua.edu.cn>
2014-03-29 11:51:08 +0800

    3.29

--- Day3/src/Human.java
+++ Day3/src/Human.java
@@ -1,7 +1,14 @@
 //类 = 域 + 方法
 //class = field(s) + method(s)
-//test...
 public class Human {
+
+    Human(String s, int i, double d1, double d2, char c) {
+        name = s;
+        age = i;
+        weight = d1;
+        height = d2;
+        sex = c;
+    }
 
     //    域
     String name;
@@ -9,6 +16,7 @@ public class Human {
     double weight;
     double height;
     char sex;//m, f
+
 
     //    方法
     void study(int i, double d, String s) {
---
+++ Day3/src/Vehicle.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Administrator on 14-3-29.
+ */
+public class Vehicle {
+
+    String color;
+    String factory;
+    double weight;
+
+    Vehicle(String s1, String s2, double d) {
+        color = s1;
+        factory = s2;
+        weight = d;
+    }
+
+    void start() {
+
+    }
+
+    void run() {
+
+    }
+
+    void stop() {
+
+    }
+}
