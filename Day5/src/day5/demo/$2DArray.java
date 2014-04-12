9726eeb1ae444e15359cbed7954bcb49feef9bd5
teacher <test@tsinghua.edu.cn>
2014-04-12 15:11:37 +0800

    4.11

---
+++ Day5/src/day5/demo/$2DArray.java
@@ -0,0 +1,27 @@
+package day5.demo;
+
+/**
+ * Created by Administrator on 14-4-12.
+ */
+public class $2DArray {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[5][];
+        int[] row1 = new int[5];
+        matrix[0] = row1;
+        int[] row2 = new int[4];
+        matrix[1] = row2;
+        int[] row3 = new int[2];
+        matrix[2] = row3;
+        int[] row4 = new int[5];
+        matrix[3] = row4;
+        int[] row5 = new int[3];
+        matrix[4] = row5;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = row + col;
+            }
+        }
+        System.out.println(matrix[matrix.length - 1][matrix[matrix.length-1].length - 1]);
+    }
+}
---
+++ Day5/src/day5/demo/ArrayTest.java
@@ -0,0 +1,47 @@
+package day5.demo;
+
+/**
+ * Created by Administrator on 14-4-12.
+ */
+public class ArrayTest {
+
+    private static final int[] X_ARRAY = {1, -1, 0};//
+    private static final int X = 123;
+
+    public static void main(String[] args) {
+        int[] c = new int[12];
+        c[11] = 100;
+        System.out.println(c[11]);
+        System.out.println(c.length);
+
+        String[] strings = new String[100];
+        System.out.println(strings[strings.length - 1]);
+
+        boolean[] booleans = new boolean[10];
+        System.out.println(booleans[0]);
+
+        double[] squares = new double[10];
+
+//        itar iterate array
+        System.out.println("---------------");
+        for (int i = 0; i < squares.length; i++) {
+            squares[i] = i * i;
+
+            System.out.println(squares[i]);
+        }
+        System.out.println("---------------");
+        System.out.println(squares);// 81 100 0
+
+        int[] n = {1, -2345, 5, 689};
+        n[3] = 100;
+        System.out.println(n[3]);
+
+        X_ARRAY[0] = 1000;
+
+        System.out.println(X_ARRAY[2]);
+
+//        X = 456;
+
+
+    }
+}
